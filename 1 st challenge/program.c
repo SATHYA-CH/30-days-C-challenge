e74fc26e9490cb62f281d3982b846d879c3665f9
SATHYA-CH <chsrksathya@gmail.com>
2026-09-23 10:08:56 +0530

    Update program.c

--- 1 st challenge/program.c
+++ 1 st challenge/program.c
@@ -6,7 +6,7 @@ printf("Enter marks of 5 subjects");
 scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
 total=m1+m2+m3+m4+m5;
 avg=total/500;
-per=(total/500)*100
+per=(total/500)*100;
 printf("total is %f \n,avg is %f \n,per is %f",total,avg,per);
 return 0;
 }
